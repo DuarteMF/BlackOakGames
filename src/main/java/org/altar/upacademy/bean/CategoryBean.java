73d4fa20c488ceb6cfb057498b717b9d1baed4a0
AndrePreto <a.preto@campus.fct.unl.pt>
2017-07-11 13:23:27 +0100

    created CategoryBean

--- src/main/java/org/altar/upacademy/bean/CategoryBean.java
+++ src/main/java/org/altar/upacademy/bean/CategoryBean.java
@@ -1,5 +1,42 @@
 package org.altar.upacademy.bean;
 
-public class CategoryBean {
+import java.io.Serializable;
+import java.util.List;
 
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.altar.upacademy.model.Category;
+import org.altar.upacademy.repository.CategoryRepository;
+
+
+@Named("categoryBean")
+@RequestScoped
+public class CategoryBean implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private CategoryRepository categoryRepository;
+	
+	public List<Category> getCategoryList(){
+		return categoryRepository.getDbCategories();
+	}
+	
+	private Category newCategory;
+	
+	public Category getNewCategory() {
+		return newCategory;
+	}
+
+	public void setNewCategory(Category newCategory) {
+		this.newCategory = newCategory;
+	}
+
+
+	public void addToCategoryList(){
+		categoryRepository.addToDb(newCategory);
+	}
+	
 }
