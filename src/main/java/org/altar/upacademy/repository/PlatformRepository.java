ba0727b02777af26079645f3f3909ea41ecc9f9d
Mexxiax <miguelsoares91@gmail.com>
2017-07-11 12:19:34 +0100

    Update PlatformRepository

--- src/main/java/org/altar/upacademy/repository/PlatformRepository.java
+++ src/main/java/org/altar/upacademy/repository/PlatformRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Named;
 import javax.persistence.Query;
-import javax.transaction.Transactional;
+
 
 import org.altar.upacademy.model.Platform;
 
@@ -13,19 +13,16 @@ import org.altar.upacademy.model.Platform;
 @Named("platformRepository")
 @ApplicationScoped
 public class PlatformRepository extends EntityRepository<Platform> {
-	public List<Platform> getDbElements() {
+	
+	public List<Platform> getDbPlatforms() {
 		Query query = getDbConnection().createQuery("FROM Platform");
-		List<Platform> dbElements = (List<Platform>) query.getResultList();
-		return dbElements;
+		List<Platform> dbPlatforms = (List<Platform>) query.getResultList();
+		return dbPlatforms;
 	}
 //@Override
 //@Transactional
 //public void removeFromDb(Platform platform){
-<<<<<<< HEAD
-//	Platpform platformToRemove = getDb().find(Platform.class, platform.getId());
-=======
 //	Platform platformToRemove = getDb().find(Platform.class, platform.getId());
->>>>>>> dev
 //	getDb().remove(platformToRemove);
 //	}
 //	
@@ -34,9 +31,4 @@ public class PlatformRepository extends EntityRepository<Platform> {
 //	Platform dbPlatform = getDb().find(Platform.class, id);
 //	dbPlatform.setName(name);
 //	}
-<<<<<<< HEAD
-//
-=======
-
->>>>>>> dev
 }
