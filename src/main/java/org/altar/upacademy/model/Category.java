35a5d051b5ca51f67230183f2893594e1f469041
AndrePreto <a.preto@campus.fct.unl.pt>
2017-07-11 11:21:11 +0100

    Inicio

--- src/main/java/org/altar/upacademy/model/Category.java
+++ src/main/java/org/altar/upacademy/model/Category.java
@@ -1,5 +1,35 @@
 package org.altar.upacademy.model;
 
-public class Category {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Table;
+
+
+@javax.persistence.Entity
+@Table(name="CATEGORIES")
+public class Category implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Column(name="CategoriaId")
+	private Integer categoryId = 0;
+	@Column(name="CategoriaNome")
+	private String categoryName = "CategoryName";
+	
+	
+	public Integer getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(Integer categoryId) {
+		this.categoryId = categoryId;
+	}
+	public String getCategoryName() {
+		return categoryName;
+	}
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	
+	
 }
