df4d43e542adafe7fff7d8a06770bdecb762ab86
DuarteMF <duartepferreira@hotmail.com>
2017-07-11 10:52:29 +0100

    creating entity repository

--- src/main/java/org/altar/upacademy/repository/EntityRepository.java
+++ src/main/java/org/altar/upacademy/repository/EntityRepository.java
@@ -1,7 +1,37 @@
 package org.altar.upacademy.repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
 import org.altar.upacademy.model.Entity;
 
 public class EntityRepository<E extends Entity> {
 
+	@PersistenceContext(unitName="database")
+	private EntityManager em;
+	
+	public EntityManager getDbConnection(){
+		return em;
+	}
+	
+	@Transactional
+	public void addToDb(E entity){
+		em.persist(entity);
+	}
+	
+	@Transactional
+	public void readFromDb(E entity){
+		em.find(Entity.class, entity);
+	}
+	
+	@Transactional
+	public void updateInDb(E entity){
+		em.merge(entity);
+	}
+	
+	@Transactional
+	public void removeFromDb(E entity){
+		em.remove(entity);
+	}
 }
