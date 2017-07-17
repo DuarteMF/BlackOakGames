0afaeec5bf98f4a9127975a6ee591928fc27a1dd
DuarteMF <duartepferreira@hotmail.com>
2017-07-17 10:20:07 +0100

    fixed an issue with product edit (if you change a products name, it can't be used anymore,ID is necessary for these operations)

--- src/main/java/org/altar/upacademy/bean/ProductBean.java
+++ src/main/java/org/altar/upacademy/bean/ProductBean.java
@@ -78,25 +78,25 @@ public class ProductBean implements Serializable {
 		Set<String> platformSetNames = platformSet.stream().map(n->n.getPlatformName()).collect(Collectors.toSet());
 		for(Category category: existingCategories()){
 			Set<Product> productSetTemp = category.getProductSet();
-			List<String> productSetNames = productSetTemp.stream().map(n->n.getProductName()).collect(Collectors.toList());
-			if(productSetNames.contains(editedProduct.getProductName()) && !categorySetNames.contains(category.getCategoryName())){
-				productSetNames.remove(editedProduct.getProductName());
-			}else if(!productSetNames.contains(editedProduct.getProductName()) && categorySetNames.contains(category.getCategoryName())){
-				productSetNames.add(editedProduct.getProductName());
+			List<Integer> productSetIds = productSetTemp.stream().map(n->n.getProductId()).collect(Collectors.toList());
+			if(productSetIds.contains(editedProduct.getProductId()) && !categorySetNames.contains(category.getCategoryName())){
+				productSetIds.remove(editedProduct.getProductId());
+			}else if(!productSetIds.contains(editedProduct.getProductId()) && categorySetNames.contains(category.getCategoryName())){
+				productSetIds.add(editedProduct.getProductId());
 			}
-			Set<Product> newProductSetTemp = productRepository.getProductsFromNames(productSetNames);
+			Set<Product> newProductSetTemp = productRepository.getProductsFromIds(productSetIds);
 			category.setProductSet(newProductSetTemp);
 			categoryRepository.updateInDb(category);
 		}
 		for(Platform platform: existingPlatforms()){
 			Set<Product> productSetTemp = platform.getProductSet();
-			List<String> productSetNames = productSetTemp.stream().map(n->n.getProductName()).collect(Collectors.toList());
-			if(productSetNames.contains(editedProduct.getProductName()) && !platformSetNames.contains(platform.getPlatformName())){
-				productSetNames.remove(editedProduct.getProductName());
-			}else if(!productSetNames.contains(editedProduct.getProductName()) && platformSetNames.contains(platform.getPlatformName())){
-				productSetNames.add(editedProduct.getProductName());
+			List<Integer> productSetIds = productSetTemp.stream().map(n->n.getProductId()).collect(Collectors.toList());
+			if(productSetIds.contains(editedProduct.getProductId()) && !platformSetNames.contains(platform.getPlatformName())){
+				productSetIds.remove(editedProduct.getProductId());
+			}else if(!productSetIds.contains(editedProduct.getProductId()) && platformSetNames.contains(platform.getPlatformName())){
+				productSetIds.add(editedProduct.getProductId());
 			}
-			Set<Product> newProductSetTemp = productRepository.getProductsFromNames(productSetNames);
+			Set<Product> newProductSetTemp = productRepository.getProductsFromIds(productSetIds);
 			platform.setProductSet(newProductSetTemp);
 			platformRepository.updateInDb(platform);			
 		}
