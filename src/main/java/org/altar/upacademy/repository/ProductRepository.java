93c6b91ffdfd6b310c000cd11fd5fc7c27a23c51
DuarteMF <duartepferreira@hotmail.com>
2017-07-18 12:36:31 +0100

    trying to create product page (initial attempt)

--- src/main/java/org/altar/upacademy/repository/ProductRepository.java
+++ src/main/java/org/altar/upacademy/repository/ProductRepository.java
@@ -39,4 +39,12 @@ public class ProductRepository extends EntityRepository<Product> {
 		}
 		return products;
 	}
+	
+	public Product getProductFromId(Integer productId) {
+		TypedQuery<Product> query = getDbConnection().createQuery("SELECT p FROM Product AS p WHERE p.productId = :id",
+				Product.class);
+		query.setParameter("id", productId);
+		List<Product> results = query.getResultList();
+		return results.get(0);
+	}
 }
